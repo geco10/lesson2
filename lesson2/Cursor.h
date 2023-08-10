b5ac3c91decb5b41a410847b97488b87798e09d5
Dilena Koplande <koplande@yahoo.com>
2023-08-10 12:39:20 -0500

    yhu

--- lesson2/Cursor.h
+++ lesson2/Cursor.h
@@ -1,15 +1,67 @@
 #pragma once
-#include<cstdio>
+#include<iostream>
 #include<vector>
+template <typename ElementType>
 class Cursor
 {
-	int num=0;
+	ElementType num=0;
 	int in, jn;
-	std::vector<std::vector<int>>arr;
+	std::vector<std::vector<ElementType>>arr;
 public:
 	Cursor(int n,int m);
 	void print();
 	void move(int i,int j);
 	bool check();
-	void cross(int a);
+	void cross(ElementType a);
 };
+
+template<typename ElementType>
+Cursor<ElementType>::Cursor(int n, int m) :arr(n)
+{
+	in = 0;
+	jn = 0;
+	ElementType c = 0;
+	for (std::vector<ElementType>& vec : arr)
+	{
+		vec.resize(m);
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++, c++) {
+			arr[i][j] = c;
+		}
+	}
+}
+template<typename ElementType>
+void Cursor<ElementType>::print()
+{
+	for (int i = 0; i < arr.size(); i++) {
+		for (int j = 0; j < arr[i].size(); j++) {
+			std::cout << arr[i][j]<<'\t';
+		}
+		puts("");
+	}
+}
+template<typename ElementType>
+void Cursor<ElementType>::move(int i, int j)
+{
+	arr[in][jn] = num;
+	in += i;
+	jn += j;
+	num = arr[in][jn];
+	arr[in][jn] = 0;
+}
+template<typename ElementType>
+bool Cursor<ElementType>::check()
+{
+	if (in == arr.size() - 1 || jn == arr[in].size() - 1 || in == 0 || jn == 0)return false;
+	return true;
+}
+
+template<typename ElementType>
+void Cursor<ElementType>::cross(ElementType a)
+{
+	arr[in + 1][jn] = a;
+	arr[in - 1][jn] = a;
+	arr[in][jn + 1] = a;
+	arr[in][jn - 1] = a;
+}
--- lesson2/lesson2.cpp
+++ lesson2/lesson2.cpp
@@ -1,8 +1,8 @@
-#include<cstdio>
+#include<iostream>
 #include"Cursor.h"
-#include"CursorF.h"
-void workInt(int n,int m) {
-	Cursor cursor(n, m);
+template<typename Type>
+void work(int n,int m) {
+	Cursor<Type> cursor(n, m);
 	while (true) {
 		cursor.print();
 		printf("Enter comand:");
@@ -18,8 +18,8 @@ void workInt(int n,int m) {
 					else
 						if (c == 5) {
 							printf("     Enter new element:");
-							int a;
-							scanf_s("%i", &a);
+							Type a;
+							std::cin >> a  ;
 							if (!cursor.check())
 								puts("\n    Error");
 							else
@@ -29,34 +29,7 @@ void workInt(int n,int m) {
 							if (c == 0)break;
 	}
 }
-void workFloat(int n,int m) {
-	CursorF cursor(n,m);
-	while (true) {
-		cursor.print();
-		printf("Enter comand:");
-		int c;
-		scanf_s("%i", &c);
-		if (c == 2) cursor.move(0, 1);
-		else
-			if (c == 1) cursor.move(0, -1);
-			else
-				if (c == 4) cursor.move(1, 0);
-				else
-					if (c == 3) cursor.move(-1, 0);
-					else
-						if (c == 5) {
-							printf("     Enter new element:");
-							float a;
-							scanf_s("%f", &a);
-							if (!cursor.check())
-								puts("\n    Error");
-							else
-								cursor.cross(a);
-						}
-						else
-							if (c == 0)break;
-	}
-}
+
 int main() {
 	int n, m;
 	int mode;
@@ -67,7 +40,7 @@ int main() {
 	scanf_s("%i",&mode);
 	puts("");
 	if (mode == 1)
-		workInt(n,m);
+		work<int>(n,m);
 	if (mode == 2)
-		workFloat(n,m);
+		work<float>(n,m);
 }
